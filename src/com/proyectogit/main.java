3d4922745c9a9574b219fff60da29b582978b87d
Gabriel Domínguez Reyes <gdominguezreyes@danielcastelao.com>
2020-10-30 10:37:41 +0100

    Primer commit

---
+++ src/com/proyectogit/main.java
@@ -0,0 +1,7 @@
+package com.proyectogit;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("primeras cositas");
+    }
+}
---
+++ src/com/proyectogit/primerproyecto/helloworld.java
@@ -0,0 +1,7 @@
+package com.proyectogit.primerproyecto;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
